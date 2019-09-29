a43b2cfa2b73a032c662839654add2be73039825
tdmarchetta <tdmarchetta@gmail.com>
2019-09-28 19:46:21 -0700

    CRS

--- Setup.ps1
+++ Setup.ps1
@@ -1,22 +1,22 @@
-﻿# PowerShell To Setting up Scripts to talk to Certify The Web.
+crsadmin# PowerShell To Setting up Scripts to talk to Certify The Web.
 # Author: Taylor D. Marchetta
 
 $currentlocation = Get-Location
-New-Item -Path "C:\" -Name "TXTAdder" -ItemType "directory"
+New-Item -Path "C:\" -Name "crsadmin" -ItemType "directory"
 
 # Copy file(s) from the location where "Setup.ps1" is at.
-Copy-Item -Path "$currentlocation\CTW_TXT_Add.bat" -Destination 'C:\TXTAdder\'
-Copy-Item -Path "$currentlocation\CTW_TXT_Add.ps1" -Destination 'C:\TXTAdder\'
-Copy-Item -Path "$currentlocation\CTW_TXT_Delete.bat" -Destination 'C:\TXTAdder\'
-Copy-Item -Path "$currentlocation\CTW_TXT_Delete.ps1" -Destination 'C:\TXTAdder\'
-Copy-Item -Path "$currentlocation\RDGateway_Update.ps1" -Destination 'C:\TXTAdder\'
+Copy-Item -Path "$currentlocation\CTW_TXT_Add.bat" -Destination 'C:\crsadmin\'
+Copy-Item -Path "$currentlocation\CTW_TXT_Add.ps1" -Destination 'C:\crsadmin\'
+Copy-Item -Path "$currentlocation\CTW_TXT_Delete.bat" -Destination 'C:\crsadmin\'
+Copy-Item -Path "$currentlocation\CTW_TXT_Delete.ps1" -Destination 'C:\crsadmin\'
+Copy-Item -Path "$currentlocation\RDGateway_Update.ps1" -Destination 'C:\crsadmin\'
 
 # $getAPIKey | Add-Content -Path 
 $getAPIKey = Read-Host -Prompt 'Dynu API Key, Please'
-$getAPIKey | Add-Content -Path "C:\TXTAdder\DynuAPIKey.txt"
+$getAPIKey | Add-Content -Path "C:\crsadmin\DynuAPIKey.txt"
 
 # These PowerShell scripts are not signed...
 Set-ExecutionPolicy Bypass
 
-# Unblock all files in TXTAdder
-Get-ChildItem C:\TXTAdder\ | Unblock-File
+# Unblock all files in crsadmin
+Get-ChildItem C:\crsadmin\ | Unblock-File
