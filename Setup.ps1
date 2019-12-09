5c00215491a7491fbd1f730374c69d8642423c86
tdmarchetta <tdmarchetta@gmail.com>
2019-12-09 13:27:24 -0800

    Clean up file. V 1.0.4
    Test-Path

--- Setup.ps1
+++ Setup.ps1
@@ -1,23 +1,18 @@
-﻿# PowerShell To Setting up Scripts to talk to Certify The Web.
-# Version 1.0.3
+﻿# PowerShell To Setting up Scripts to talk to Certify The Web & Dynu.
+# Version 1.0.4
 # Author: Taylor D. Marchetta
 
 # Variables
-
-# Get's the location of the executed PowerShell script and turns it into a valuable
 $currentlocation = Get-Location
-$crsadmin = "C:\crsadmin\"
 $crsadminTXT = "C:\crsadmin\TXTAdder\"
 
 # Check for 'C:\crsadmin' folder.
-if (!(Test-Path '$crsadmin')) {
+# '.trim' will remove leading and trailing spaces and leading and trailing line breaks
+if (!(test-path $crsadminTXT.trim())) {
 
-    # Makes new folder "TXTAdder"
-    New-Item -Path "C:\" -Name "crsadmin" -ItemType Directory
+    # Makes new folder for crsadmin and/or TXTAdder: C:\crsadmin\TXTAdder
+    New-Item -Path "$crsadminTXT" -ItemType Directory
 }
-
-# Makes new folder "TXTAdder"
-New-Item -Path "C:\crsadmin" -Name "TXTAdder" -ItemType Directory
 
 # Copy file(s) from the location where "Setup.ps1" is at.
 Copy-Item -Path "$currentlocation\CRS_*.*" -Destination '$crsadminTXT' -Recurse
@@ -31,5 +26,3 @@ Set-ExecutionPolicy Bypass
 
 # Unblock all files in TXTAdder
 Get-ChildItem '$crsadminTXT' | Unblock-File
-
-# “The only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it” - Steve Jobs
