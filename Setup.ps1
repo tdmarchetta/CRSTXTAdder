d2d7e34bbafa1a26854bd91a2230bcef94f61ab5
tdmarchetta <tdmarchetta@gmail.com>
2019-12-09 11:27:23 -0800

    Cleaned up Copy-Item -Path

--- Setup.ps1
+++ Setup.ps1
@@ -6,10 +6,11 @@
 
 # Get's the location of the executed PowerShell script and turns it into a valuable
 $currentlocation = Get-Location
-
+$crsadmin = "C:\crsadmin\"
+$crsadminTXT = "C:\crsadmin\TXTAdder\"
 
 # Check for 'C:\crsadmin' folder.
-if (!(Test-Path 'C:\crsadmin')) {
+if (!(Test-Path '$crsadmin')) {
 
     # Makes new folder "TXTAdder"
     New-Item -Path "C:\" -Name "crsadmin" -ItemType Directory
@@ -19,20 +20,16 @@ if (!(Test-Path 'C:\crsadmin')) {
 New-Item -Path "C:\crsadmin" -Name "TXTAdder" -ItemType Directory
 
 # Copy file(s) from the location where "Setup.ps1" is at.
-Copy-Item -Path "$currentlocation\CRS_CTW_TXT_Add.bat" -Destination 'C:\crsadmin\TXTAdder\' 
-Copy-Item -Path "$currentlocation\CRS_CTW_TXT_Add.ps1" -Destination 'C:\crsadmin\TXTAdder\'
-Copy-Item -Path "$currentlocation\CRS_CTW_TXT_Delete.bat" -Destination 'C:\crsadmin\TXTAdder\'
-Copy-Item -Path "$currentlocation\CRS_CTW_TXT_Delete.ps1" -Destination 'C:\crsadmin\TXTAdder\'
-Copy-Item -Path "$currentlocation\CRS_RDGateway_Update.ps1" -Destination 'C:\crsadmin\TXTAdder\'
+Copy-Item -Path "$currentlocation\CRS_*.*" -Destination '$crsadminTXT' -Recurse
 
 # Gets APIKey | Add-Content -Path 
 $getAPIKey = Read-Host -Prompt 'Dynu API Key, Please'
-$getAPIKey | Add-Content -Path "C:\crsadmin\TXTAdder\DynuAPIKey.txt"
+$getAPIKey | Add-Content -Path "$crsadminTXT\DynuAPIKey.txt"
 
 # These PowerShell scripts are not signed...
 Set-ExecutionPolicy Bypass
 
 # Unblock all files in TXTAdder
-Get-ChildItem "C:\crsadmin\TXTAdder\" | Unblock-File
+Get-ChildItem '$crsadminTXT' | Unblock-File
 
 # “The only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it” - Steve Jobs
