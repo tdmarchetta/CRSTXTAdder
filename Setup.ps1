775286e881a56947a205e4c1de764afdd975d27d
tdmarchetta <tdmarchetta@gmail.com>
2021-02-14 06:57:20 -0800

    Clean up text and comments

--- Setup.ps1
+++ Setup.ps1
@@ -1,5 +1,5 @@
 ﻿# PowerShell To Setting up Scripts to talk to Certify The Web & Dynu.
-# Version 1.0.5
+# Version 1.0.6
 # Author: Taylor D. Marchetta
 
 # Variables
@@ -10,19 +10,19 @@ $crsadminTXT = "C:\crsadmin\TXTAdder\"
 # '.trim' will remove leading and trailing spaces and leading and trailing line breaks
 if (!(test-path $crsadminTXT.trim())) {
 
-    # Makes new folder for crsadmin and/or TXTAdder: C:\crsadmin\TXTAdder
+    # Makes new folder for crsadmin: C:\crsadmin\TXTAdder
     New-Item -Path "$crsadminTXT" -ItemType Directory
 }
 
 # Copy file(s) from the location where "Setup.ps1" is at.
 Copy-Item -Path "$currentlocation\CRS_*" -Destination "$crsadminTXT" -Recurse -Force
 
-# Gets APIKey | Add-Content -Path 
+# $getAPIKey | Add-Content -Path - PowerShell will ask for the Dynu API Key.
 $getAPIKey = Read-Host -Prompt 'Dynu API Key, Please'
 $getAPIKey | Add-Content -Path "$crsadminTXT\DynuAPIKey.txt"
 
 # These PowerShell scripts are not signed...
 Set-ExecutionPolicy Bypass
 
-# Unblock all files in TXTAdder
+# Unblock all files in CRSTXTAdder
 Get-ChildItem "$crsadminTXT" | Unblock-File
